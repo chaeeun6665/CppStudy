9ea6116166cf84e8e646c510e90287ae6f00f231
Chaeeun Lee <jenny6665@naver.com>
2025-06-11 01:43:57 +0900

    feat: add explanation for getline

--- chapter2/exercise_1.cpp
+++ chapter2/exercise_1.cpp
@@ -6,7 +6,7 @@
 //이름과 주소를 출력하는 C++ 프로그램을 작성하시오.
 
 #include <iostream>
-#include <string>
+#include <string> //string 사용위함
 
 using namespace std;
 
@@ -18,10 +18,10 @@ int main()
 	cout << "이름과 주소를 입력하세요." << endl;
 	cout << "이름 : ";
 	// cin >> name;
-	getline(cin, name);
+	getline(cin, name); //공백 포함 문자열을 받기 위해서는 getline 사용해야 한다.
 	cout << "주소 : ";
-	getline(cin, address);
 	// cin >> address;
+	getline(cin, address);
 
 	cout << "입력된 이름은 " << name << ", 주소는 " << address << endl;
 }
