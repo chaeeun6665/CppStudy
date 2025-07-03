29bea905dc4357dd4e9200ebc305512924a78256
Chaeeun Lee <jenny6665@naver.com>
2025-07-04 04:07:50 +0900

    feat : complete solution for chapter 4 / exercise 7

--- chapter4/exercise_7.cpp
+++ chapter4/exercise_7.cpp
@@ -12,39 +12,22 @@
 using namespace std;
 
 int main() {
-    // struct Pizza {
-    //     char company_name[30];
-    //     float diameter;
-    //     float weight;
-    // };
-    //
-    // Pizza pizza;
-    //
-    // cout << "피자 회사 이름 :  ";
-    // cin.getline(pizza.company_name, 30);
-    // cout << "피자 지름 : ";
-    // cin >> pizza.diameter;
-    // cout << "피자 중량 : ";
-    // cin >> pizza.weight;
-    //
-    // cout << "저장 내용 : ";
-    // cout << pizza.company_name << ", " << pizza.diameter <<", " << pizza.weight << "\n";
-
     struct Pizza {
         char company_name[30];
         float diameter;
         float weight;
     };
 
-    Pizza *pizza = new Pizza;
+    Pizza pizza;
 
     cout << "피자 회사 이름 :  ";
-    cin.getline(pizza -> company_name, 30);
+    cin.getline(pizza.company_name, 30);
     cout << "피자 지름 : ";
-    cin >> pizza -> diameter;
+    cin >> pizza.diameter;
     cout << "피자 중량 : ";
-    cin >> pizza -> weight;
+    cin >> pizza.weight;
 
     cout << "저장 내용 : ";
-    cout << pizza->company_name << ", " << pizza->diameter <<", " << pizza->weight << "\n";
+    cout << pizza.company_name << ", " << pizza.diameter <<", " << pizza.weight << "\n";
+
 }
